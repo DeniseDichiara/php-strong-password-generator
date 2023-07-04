7095bb57b4b62dfbf174cdcba77e98d6d4a18130
Denise-Dichiara <dichiara.d@gmail.com>
2023-07-04 15:53:37 +0200

    added radio and check buttons

--- index.php
+++ index.php
@@ -1,34 +1,52 @@
-
-
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Password Generator</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!--  Bootstrap-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-        <link rel="stylesheet" href="">
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Password Generator</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--  Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="">
+</head>
 
 
-    <body>
+<body>
 
     <h1>
         Strong Password Generator
     </h1>
-        
+
     <form action="" method="get">
         <label for="user-password">
             Lunghezza password:
         </label>
         <input type="text" name="user-password">
+        <br>
+        <label for="user-password">
+            Consenti ripetizioni di uno o più caratteri:
+        </label>
+        <input type="radio" name="user-password" value="answer">
+        <label for="Sì">Sì</label><br>
+        <input type="radio" name="user-password" value="answer">
+        <label for="No">No</label><br>
+
+        <input type="checkbox" name="lettere" value="lettere">
+        <label for="vehicle1">Lettere</label><br>
+
+        <input type="checkbox" name="numeri" value="numeri">
+        <label for="vehicle1">Numeri</label><br> 
+        
+        <input type="checkbox" name="simboli" value="simboli">
+        <label for="vehicle1">Simboli</label><br>
     </form>
 
 
-        <script src="" async defer></script>
-    </body>
+
+    <script src="" async defer></script>
+</body>
+
 </html>
