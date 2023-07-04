57cc764e6607f05d9b19f67c671ade9ccf26ed3f
Denise-Dichiara <dichiara.d@gmail.com>
2023-07-04 15:34:49 +0200

    label

---
+++ index.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Password Generator</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--  Bootstrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="">
+    </head>
+
+
+    <body>
+
+    <h1>
+        Strong Password Generator
+    </h1>
+        
+    <form action="" method="get">
+        <label for="user-password">
+            Lunghezza password:
+        </label>
+        <input type="text" name="user-password">
+    </form>
+
+
+        <script src="" async defer></script>
+    </body>
+</html>
