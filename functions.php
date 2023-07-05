848a249193202bd3489fa628cde134b1b03d417c
Denise-Dichiara <dichiara.d@gmail.com>
2023-07-05 08:51:20 +0200

    added new password

--- functions.php
+++ functions.php
@@ -6,3 +6,28 @@ $newPassword  = $_GET['userPassword'];
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>New Password</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+
+
+<body>
+    
+    <p>
+        "<?php echo $newPassword; ?>"
+    </p>
+
+
+    <script src="" async defer></script>
+</body>
+
+</html>
