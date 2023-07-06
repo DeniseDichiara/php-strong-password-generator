e746776b2f471bf58a7a69ed077c38683e97b839
Denise-Dichiara <dichiara.d@gmail.com>
2023-07-06 15:01:32 +0200

    created random password

--- index.php
+++ index.php
@@ -1,90 +1,20 @@
-<?php
+<?php 
 
+if ( isset ($_GET['pswLength'])) {
+    //*comportamneti che avvengono dopo la richiesta
+}
+function getRandomPassword($passwordLength){
 
-$letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-$numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'];
-$symbols = ['!', '"', '?', '$', '%', '^', '&', '*', '(', ')',  '_', '-', '+', '=', '{', '[', '}', ']', ':', ';', '@', '~', '#', '|', '<', ','];
+    $characters  = 'qwertyuiopasdfghjklzxcvbnm';
+    $numebers = '1234567890';
+    $symbols = '!@#$%&()_+{}|:"<>?[]\;.';
 
-$result = [];
-//var_dump($result);
+    $actualCharacters = strtoupper($characters) .  $characters . $numebers .  $symbols;
+    return $actualCharacters; 
 
-function newPasswordGenerator($letters, $numbers, $symbols ){
-    for ($i = 0; $i < count($letters); $i++) {
-        array_push($result, $letters[$i], $numbers[$i], $symbols[$i]);
-    }
+    $newPassword = '';
 }
 
-
-
-
-
+echo getRandomPassword(1);
 
 ?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Password Generator</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!--  Bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="">
-</head>
-
-
-<body>
-
-    <h1>
-        Strong Password Generator
-    </h1>
-
-    <form>
-        <input type="text" name="userPassword" class="form-control mb-3" aria-describedby="passwordHelpBlock" placeholder="nessun paramentro valido inserito">
-        </div>
-
-    </form>
-
-    <form action="./function.php" method="get">
-        <label for="user-password">
-            Lunghezza password:
-        </label>
-        <input type="password" id="inputPassword5"  >
-        <br>
-
-        <!--
-        <p>
-            "<?php echo $newPassword; ?>"
-        </p>
-        <br>
-        -->
-
-        <label for="user-password">
-            Consenti ripetizioni di uno o più caratteri:
-        </label>
-        <input type="radio" name="user-password" value="answer">
-        <label for="Sì">Sì</label><br>
-
-        <input type="radio" name="user-password" value="answer">
-        <label for="No">No</label><br>
-
-        <input type="checkbox" name="lettere" value="lettere">
-        <label for="vehicle1">Lettere</label><br>
-
-        <input type="checkbox" name="numeri" value="numeri">
-        <label for="vehicle1">Numeri</label><br>
-
-        <input type="checkbox" name="simboli" value="simboli">
-        <label for="vehicle1">Simboli</label><br>
-    </form>
-
-
-
-    <script src="" async defer></script>
-</body>
-
-</html>
